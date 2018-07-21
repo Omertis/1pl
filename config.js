2ecdec9acf9466033d291e2f2d5d0bacaa2993e5
Omertis <36013948+Omertis@users.noreply.github.com>
2018-07-21 10:00:14 +0300

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-exports.TOKEN = 'NDcwMTExNTY3NDUxNzgzMTc4.DjRhkg.aT1zl6Df3zd6ArWy-GCINatkIec'; // ya wr3 la tns5
-
-exports.PREFIX = '1';
-
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
