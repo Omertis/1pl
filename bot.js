3ea641a4e45fe8ec51ab18252243120ff7d2ace3
Omertis <36013948+Omertis@users.noreply.github.com>
2018-07-21 03:54:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const fetchVideoInfo = require('youtube-info');
 
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '1';
-const discord_token = "NDcwMDI2NTYwMDUzNzA2NzUz.DjQSnw.UTFxG3Fa-HW4QTsPrhty_RSVo7o ";
+const discord_token = "NDcwMDI2NTYwMDUzNzA2NzUz.DjQSnw.UTFxG3Fa-HW4QTsPrhty_RSVo7o";
 client.login(discord_token);
 client.on('ready', function() {
 	console.log(`i am ready ${client.user.username}`);
